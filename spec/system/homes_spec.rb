1ccf904579ce4051459d9202ec9d4c56dbfa8a3c
FUNABARA Masao <masao@masoo.jp>
2021-07-01 04:37:06 +0000

    success

--- spec/system/homes_spec.rb
+++ spec/system/homes_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :system do
   it "GET /" do
-    #visit "/"  # success.
+    Capybara.app = Rack::Builder.parse_file(Rails.root.join('config.ru').to_s).first
+    # visit "/"  # success.
     visit "/i/am/users/" # fail.
     expect(page).to have_content "ここはホーム"
   end
